f8d4694e36925848b4b3c047c2fad7d5af36cb2e
あさると <34514603+Assault1892@users.noreply.github.com>
2022-09-15 08:48:16 +0900

    古い命名規則に合うように変更

--- picture_rename_tool.ps1
+++ picture_rename_tool.ps1
@@ -3,9 +3,9 @@ $count = ($childItems | Measure-Object).count
 $index = 0
 foreach($file in $childItems) {
     $index++
-    if ($file.name -match "VRChat_[0-9]+x[0-9]+_[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}\.[0-9]{3}\.png") {
+    if ($file.name -match "VRChat_[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}\.[0-9]{3}_[0-9]+x[0-9]+\.png") {
         $split = $file.name.split("_.")
-        $newName = $split[0] + "_" + $split[2] + "_" + $split[3] + "." + $split[4] + "_" + $split[1] + "." + $split[5]
+        $newName = $split[0] + "_" + $split[4] + "_" + $split[1] + "_" + $split[2] + "." + $split[3] + "." + $split[5]
         $progress = "{0:P}" -f ($index/$count)
         Rename-Item -Path $file.fullName -NewName $newName
         Write-Host "`r$newName - $progress ($index/$count)" -NoNewline
